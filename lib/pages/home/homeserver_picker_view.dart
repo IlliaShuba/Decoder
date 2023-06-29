3d327e89194a04853ab2d4158a6d23958ee86e27
Illya Shuba <105970858+IlliaShuba@users.noreply.github.com>
2023-06-29 11:01:09 +0300

    homepage redesign

--- lib/pages/home/homeserver_picker_view.dart
+++ lib/pages/home/homeserver_picker_view.dart
@@ -1,11 +1,7 @@
-import 'dart:ui';
-
 import 'package:decoder/pages/home/homeserver_picker.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:url_launcher/url_launcher_string.dart';
-
 import '../../utils/custom_scroll_behavior.dart';
 
 
@@ -19,121 +15,98 @@ class HomeserverPickerView extends StatelessWidget {
     double baseWidth = 360;
     double baseHeight = 800;
     double fem = MediaQuery.of(context).size.width / baseWidth;
-    double fvm = MediaQuery.of(context).size.height / baseHeight;
-    double ffem = fem * 0.97;
-
+    double fhm = MediaQuery.of(context).size.height / baseHeight;
     return Scaffold(
-      body: SizedBox(
-        width: baseHeight * fem,
-        height: baseHeight * fvm,
+      body: Container(
+        // androidlarge99Xz (1:1043)
+        padding: EdgeInsets.fromLTRB(20*fem, 140*fhm, 20*fem, 100*fhm),
+        width: double.infinity,
+        decoration: BoxDecoration (
+          color: Color(0xffffffff),
+        ),
         child: Column(
-          mainAxisAlignment:  MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              padding: EdgeInsets.fromLTRB(18*fem, 28.74*fem, 18*fem, 22*fvm),
-              width: 300*fem,
-              decoration: BoxDecoration (
-                color: const Color(0x72af9e2c),
-                borderRadius: BorderRadius.circular(20*fem),
-              ),
-              child: ClipRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur (
-                    sigmaX: 10*fem,
-                    sigmaY: 10*fem,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0.24*fem, 15.43*fem),
-                        width: 148.37*fem,
-                        height: 174.83*fem,
+              // logoALc (1:1044)
+              margin: EdgeInsets.fromLTRB(77*fem, 0*fhm, 76*fem, 25*fhm),
+              width: double.infinity,
+              height: 164*fhm,
+              child: Stack(
+                children: [
+                  Positioned(
+                    // group6f2U (1:1045)
+                    left: 0*fem,
+                    top: 0*fem,
+                    child: Align(
+                      child: SizedBox(
+                        width: 167*fem,
+                        height: 164*fhm,
                         child: Image.asset(
-                          'assets/dark107brigada-2.png',
-                          width: 148.37*fem,
-                          height: 174.83*fem,
+                          'assets/icon/logo-background.png',
+                          width: 167*fem,
+                          height: 164*fhm,
                         ),
                       ),
-                      Container(
-                        // fyo (6:12)
-                        constraints: BoxConstraints (
-                          maxWidth: 264*fem,
-                        ),
-                        child: Text(
-                          '107 окрема бригада Сил територіальної оборони Буковини\n',
-                          textAlign: TextAlign.center,
-                          style: SafeGoogleFont (
-                            'Inter',
-                            fontSize: 14*ffem,
-                            fontWeight: FontWeight.w800,
-                            height: 1.2102272851*ffem/fem,
-                            letterSpacing: 0.98*fem,
-                            color: const Color(0xff75704e),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  Positioned(
+                    // maskgroupZ7r (1:1048)
+                    left: 30.8460693359*fem,
+                    top: 29.7723999023*fhm,
+                    child: Align(
+                      child: SizedBox(
+                        width: 104.96*fem,
+                        height: 104.96*fhm,
+                        child: Image.asset(
+                          'assets/icon/lock.png',
+                          width: 104.96*fem,
+                          height: 104.96*fhm,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
-                constraints: BoxConstraints (
-                  maxWidth: 263*fem,
-                  maxHeight: 66*fvm,
-                ),
-                child: Text(
-                  'ТЕРИТОРІАЛЬНА ОБОРОНА БУКОВИНИ\nГОТОВІ ДО СПРОТИВУ',
-                  textAlign: TextAlign.center,
-                  style: SafeGoogleFont (
-                    'Inter',
-                    fontSize: 15*ffem,
-                    fontWeight: FontWeight.w800,
-                    height: 1.2102272034*ffem/fem,
-                    letterSpacing: 1.05*fem,
-                    color: const Color(0xff3c371c),
-                  ),
+              // ef6 (1:1051)
+              margin: EdgeInsets.fromLTRB(2*fem, 0*fhm, 0*fem, 306*fhm),
+              child: Text(
+                'Захищений мессенджер',
+                style: SafeGoogleFont (
+                  'Montserrat',
+                  fontSize: 14*fhm,
+                  fontWeight: FontWeight.w300,
+                  height: 1.2175*fhm,
+                  color: Color(0xff278664),
                 ),
               ),
+            ),
             ElevatedButton.icon(
               onPressed: controller.isLoading
                   ? null
                   : controller.checkHomeserverAction,
               style:ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xff75704e),
-                minimumSize: const Size(291, 42),
-                elevation: 6.0,
+                backgroundColor: const Color(0xff278664),
+                minimumSize: const Size(double.infinity, 42),
+                padding: EdgeInsets.fromLTRB(94.57*fem, 9*fhm, 90.43*fem, 9*fhm),
               ),
-              icon:  Image.asset('assets/primeng-icons-v500.png', width: 16*fem, height: 16*fvm,),
+              icon:  Image.asset('assets/icon/primeng-icons-v500.png', width: 16*fem, height: 16*fhm,),
               label:Text(
                 // 8RT (7:17)
                 L10n.of(context)!.start,
                 style: SafeGoogleFont (
                   'Inter',
-                  fontSize: 15*ffem,
+                  fontSize: 15*fhm,
                   fontWeight: FontWeight.w600,
-                  height: 1.2102272034*ffem/fem,
+                  height: 1.2102272034*fhm,
                   letterSpacing: 0.45*fem,
                   color: const Color(0xffffffff),
                 ),
               ),
             ),
-
-              /*Container(
-                // startTRo (12:1044)
-                padding: EdgeInsets.fromLTRB(4.25*fem, 3*fem, 0*fem, 3*fem),
-                width: double.infinity,
-                height: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-
-                  ],
-                ),
-              ),
-            ),*/
-         ],
+          ],
         ),
       ),
     );
